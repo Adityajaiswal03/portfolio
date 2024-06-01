eb0a8603d9fd7766be7c214fd135b34ec525ee56
Adityajaiswal03 <git@github.com:Adityajaiswal03/ProductService.git>
2024-06-01 23:50:12 +0530

    final commit

--- src/Education/Education.js
+++ src/Education/Education.js
@@ -2,19 +2,21 @@ import './Education.css';
 import logo from '../download.jpeg';
 function Education(){
     return (
-      
         <div class="timeline">
             <div class="container left">
                 <div class="content">
-                    <h2>2017</h2>
-                    <p>Lorem ipsum..</p>
+                    <h2>2027</h2>
+
+                    <p>Scaler School of Technology</p>
                 </div>
             </div>
             <div class="container right">
-                <div class="content">
-                    <h2>2016</h2>
-                    <p>Lorem ipsum..</p>
-                </div>
+                <div class="content"></div>
+                <h2>2022</h2>
+
+                <p>Senior Secondary: DAV Public School,Sector-14</p>
+
+                <p>93.4%</p>
             </div>
         </div>
     );
